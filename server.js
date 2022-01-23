bfa4496e5de16e62a4d6cc596cb4deec4e617904
haskoe <henrik@haskoe.dk>
2022-01-23 12:11:41 +0100

    dotenv

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const nodemailer = require('nodemailer')
+require('dotenv').config()
 
 const PASS = ''
 
 const smtpOptions = {
-    host: 'send.one.com',
-    port: 465,
-    user: 'henrik@.dk',
-    pass: PASS
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PWD
 }
 
 function createSmtpTransport(smtpOptions) {
